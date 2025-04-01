b81bc2fec47cb4169e8dee89ac4e1c16cfaca4a4
torunea <23toru2@gmail.com>
2025-04-01 21:46:03 +0900

    データの追加

--- config.js
+++ config.js
@@ -3,15 +3,16 @@
 // 複数のCSVスプレッドシートの設定
 const SPREADSHEET_SOURCES = [
     {
-        name: "主要データ",
+        name: "Timeline",
         url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSikAHCTE4Mmr8UGPf3oIyfOlhy4tvTMlruQdKR91ulu3mVBpUmm1MuHzTRnVtORddOPpHrs2Ua8omT/pub?output=csv'
     },
-    /*
+
     // 追加のデータソースをここに記述
     {
-        name: "追加データ1",
-        url: 'https://docs.google.com/spreadsheets/d/e/XXXX-YOUR-SECOND-SHEET-ID/pub?output=csv'
+        name: "Timeline2",
+        url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSG7K-ORBszxO5obK_mmc4N0BtaxnONpACJlzPM8fXx1thv9NJEHuthRZxzybK0XeO2MiO9n-TAGUsn/pub?output=csv'
     },
+    /*
     {
         name: "追加データ2",
         url: 'https://docs.google.com/spreadsheets/d/e/XXXX-YOUR-THIRD-SHEET-ID/pub?output=csv'
